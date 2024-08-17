e6da91b4559f72bd973963959f3b8b7cdaa622b0
alielhasan85 <131617777+alielhasan85@users.noreply.github.com>
2024-08-17 14:26:25 +0300

    user input data is working fine need to integrate firebase

--- lib/client/widgets/phone_number.dart
+++ lib/client/widgets/phone_number.dart
@@ -1,24 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:intl_phone_field/intl_phone_field.dart';
-import 'package:intl_phone_field/phone_number.dart';
-import 'package:naya_menu/theme/app_theme.dart'; // Import your AppTheme
+import 'package:country_picker/country_picker.dart';
+import 'package:naya_menu/theme/app_theme.dart'; // Ensure this imports your theme
 
 class PhoneNumberInput extends StatelessWidget {
   final TextEditingController controller;
-  final String? Function(PhoneNumber?)? validator; // Adjusted the type
+  final String? Function(String?)? validator;
 
   const PhoneNumberInput({required this.controller, this.validator, Key? key})
       : super(key: key);
 
+  void _openCountryPickerDialog(BuildContext context) {
+    showCountryPicker(
+      context: context,
+      showPhoneCode: true, // Show phone code alongside country name
+      onSelect: (Country country) {
+        controller.text = '+${country.phoneCode}'; // Set phone code
+      },
+      countryListTheme: CountryListThemeData(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(16.0),
+          topRight: Radius.circular(16.0),
+        ),
+        inputDecoration: InputDecoration(
+          hintText: 'Search your country',
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        searchTextStyle: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Define InputDecoration based on your theme
     final inputDecoration = InputDecoration(
       border: AppTheme.inputDecorationTheme.border,
       focusedBorder: AppTheme.inputDecorationTheme.focusedBorder,
       enabledBorder: AppTheme.inputDecorationTheme.enabledBorder,
       contentPadding: AppTheme.inputDecorationTheme.contentPadding ??
           const EdgeInsets.all(10.0),
-      isCollapsed: true,
       hintText: 'Enter your phone number', // Customize the hint text as needed
       hintStyle: AppTheme.inputDecorationTheme.hintStyle ??
           const TextStyle(fontSize: 14.0),
@@ -26,22 +50,42 @@ class PhoneNumberInput extends StatelessWidget {
       fillColor: AppTheme.inputDecorationTheme.fillColor ?? Colors.blue.shade50,
     );
 
-    return SizedBox(
-      width: 500,
-      child: IntlPhoneField(
-        controller: controller,
-        decoration: inputDecoration,
-        style: TextStyle(fontSize: 15.0),
-        initialCountryCode: 'US', // Set your default country code
-        onChanged: (phone) {
-          // Handle phone number changes if needed
-        },
-        onSaved: (phone) {
-          controller.text = phone?.completeNumber ?? '';
-        },
-        validator: validator,
-        disableLengthCheck: true, // Use the provided validator
-      ),
+    return Row(
+      children: [
+        GestureDetector(
+          onTap: () => _openCountryPickerDialog(context),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: AppTheme.inputDecorationTheme.border!.borderSide.color,
+                width: AppTheme.inputDecorationTheme.border!.borderSide.width,
+              ),
+              borderRadius: BorderRadius.circular(12),
+              color: AppTheme.inputDecorationTheme.fillColor ??
+                  Colors.blue.shade50,
+            ),
+            child: Row(
+              children: [
+                Text(
+                  controller.text.isNotEmpty ? controller.text : '+1',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Icon(Icons.arrow_drop_down),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(width: 10),
+        Expanded(
+          child: TextFormField(
+            controller: controller,
+            keyboardType: TextInputType.phone,
+            decoration: inputDecoration,
+            validator: validator,
+          ),
+        ),
+      ],
     );
   }
 }
