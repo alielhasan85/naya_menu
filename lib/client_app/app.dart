f61c4328b372ca4e564b97106307a3f55529b5b3
alielhasan85 <131617777+alielhasan85@users.noreply.github.com>
2024-09-03 19:11:11 +0300

    firebase working well and deploy same

--- lib/client_app/app.dart
+++ lib/client_app/app.dart
@@ -25,46 +25,46 @@ class _MyAppState extends ConsumerState<MyApp> {
   @override
   void initState() {
     super.initState();
-    _logInDevelopmentAccount();
+    //  _logInDevelopmentAccount();
   }
 
-  Future<void> _logInDevelopmentAccount() async {
-    try {
-      final String email =
-          'elhasan.ali@gmail.com'; // Replace with your development email
-      final String password =
-          'rotation'; // Replace with your development password
+  // Future<void> _logInDevelopmentAccount() async {
+  //   try {
+  //     final String email =
+  //         'elhasan.ali@gmail.com'; // Replace with your development email
+  //     final String password =
+  //         'rotation'; // Replace with your development password
 
-      UserCredential userCredential =
-          await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
+  //     UserCredential userCredential =
+  //         await FirebaseAuth.instance.signInWithEmailAndPassword(
+  //       email: email,
+  //       password: password,
+  //     );
 
-      final userId = userCredential.user!.uid;
+  //     final userId = userCredential.user!.uid;
 
-      // Fetch the user data from Firestore and set it in the UserProvider
-      await ref.read(userProvider.notifier).fetchUser(userId);
+  //     // Fetch the user data from Firestore and set it in the UserProvider
+  //     await ref.read(userProvider.notifier).fetchUser(userId);
 
-      // Assuming you have a method to fetch the venue ID associated with the user
-      final user = ref.read(userProvider);
-      if (user != null) {
-        // Fetch the venue data associated with the user
-        final venueList = await FirestoreVenue().getAllVenues(userId);
-        if (venueList.isNotEmpty) {
-          ref.read(venueProvider.notifier).setVenue(venueList.first);
-        }
-      }
+  //     // Assuming you have a method to fetch the venue ID associated with the user
+  //     final user = ref.read(userProvider);
+  //     if (user != null) {
+  //       // Fetch the venue data associated with the user
+  //       final venueList = await FirestoreVenue().getAllVenues(userId);
+  //       if (venueList.isNotEmpty) {
+  //         ref.read(venueProvider.notifier).setVenue(venueList.first);
+  //       }
+  //     }
 
-      setState(() {
-        _isLoggedIn = true;
-        _userId = userId;
-      });
-    } catch (e) {
-      print('Login failed: $e');
-      // Handle the error, e.g., show a Snackbar or dialog
-    }
-  }
+  //     setState(() {
+  //       _isLoggedIn = true;
+  //       _userId = userId;
+  //     });
+  //   } catch (e) {
+  //     print('Login failed: $e');
+  //     // Handle the error, e.g., show a Snackbar or dialog
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -93,11 +93,12 @@ class _MyAppState extends ConsumerState<MyApp> {
           child: child!,
         );
       },
-      home: Scaffold(
-        body: _isLoggedIn && _userId != null
-            ? const MainPage() // Navigate to MainPage when logged in
-            : const Center(child: CircularProgressIndicator()),
-      ),
+      home: Scaffold(body: LoadingPage()
+
+          // _isLoggedIn && _userId != null
+          //     ? const MainPage() // Navigate to MainPage when logged in
+          //     : const Center(child: CircularProgressIndicator()),
+          ),
     );
   }
 }
